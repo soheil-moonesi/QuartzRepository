b1a64d83b7e6abb0c4a2b1c00ebcf9672e8d67eb
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2024-12-02 18:31:18 +0330

    Quartz sync: Dec 2, 2024, 6:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴وبلاگ سهیل",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
