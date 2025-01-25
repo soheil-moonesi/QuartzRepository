5055c9ecc2e82401786514d7e4599f89aceb4d96
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2025-01-25 23:06:10 +0330

    Quartz sync: Jan 25, 2025, 11:06 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -93,17 +93,20 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
     if (text) {
       const segments: (string | JSX.Element)[] = []
 
-      if (fileData.dates) {
-        segments.push(formatDate(getDate(cfg, fileData)!, cfg.locale))
-      }
+      if (fileData.frontmatter) {
+        const created = fileData.frontmatter.date
+          ? formatDate(new Date(fileData.frontmatter.date), cfg.locale)
+          : null
+        const lastmod = fileData.frontmatter.lastmod
+          ? formatDate(new Date(fileData.frontmatter.lastmod), cfg.locale)
+          : null
 
-      // Display reading time if enabled
-      if (options.showReadingTime) {
-        const { minutes, words: _words } = readingTime(text)
-        const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
-          minutes: Math.ceil(minutes),
-        })
-        segments.push(displayedTime)
+        if (created) {
+          segments.push(` 📅 انتشار: ${created} `)
+        }
+        if (lastmod && created !== lastmod) {
+          segments.push(` 🔄 به‌روزرسانی: ${lastmod} `)
+        }
       }
 
       const segmentsElements = segments.map((segment) => <span>{segment}</span>)
